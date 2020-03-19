f78db1dd8ef8a9f3e383b9c1b5e41a27bec5fc0b
coolone <coolone.official@gmail.com>
2020-03-20 01:26:09 +0300

    Algolia sync functions, chats lastmessage function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,70 @@
 import * as functions from 'firebase-functions';
+import {DocumentSnapshot} from "firebase-functions/lib/providers/firestore";
+import { SearchIndex } from "algoliasearch";
+const admin = require('firebase-admin');
+const algoliasearch = require('algoliasearch');
+admin.initializeApp();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const db = admin.firestore();
+
+exports.chatsLastMessage = functions.firestore
+  .document('chats/{chatId}/messages/{messageId}')
+  .onCreate((snapshot, context) => {
+    const lastMessageModel = snapshot.data();
+
+    if (lastMessageModel !== undefined) {
+      return db.doc(`chats/${context.params.chatId}`).update({
+        lastMessage: {
+          documentId: context.params.messageId,
+          ...lastMessageModel
+        }
+      }).catch((err: any) => 'error while add chat! ' + err);
+    }
+  });
+
+const algoliaClient = algoliasearch(functions.config().algolia.appid, functions.config().algolia.apikey);
+const chatsIndex = algoliaClient.initIndex('chats');
+const messagesIndex = algoliaClient.initIndex('messages');
+
+function algoliaUpdate(index: SearchIndex, change: functions.Change<DocumentSnapshot>) {
+  if(change.before.data() === undefined) { // insertion
+    const ss = change.after;
+    const model = ss.data();
+    if(model !== undefined) {
+      model.objectID = ss.id;
+      console.log(`save object ${model}`);
+      return index.saveObject(model);
+    }
+  } else if(change.after.data() === undefined) { // deletion
+    const ss = change.before;
+    console.log(`delete object ${ss.id}`);
+    return index.deleteObject(ss.id);
+  } else { // updating
+    const ss = change.after;
+    const model = ss.data();
+    if(model !== undefined) {
+      model.objectID = ss.id;
+      console.log(`update object ${model}`);
+      return index.partialUpdateObject(model);
+    }
+  }
+
+  console.error("something went wrong")
+  return null
+}
+
+exports.algoliaChats = functions.firestore
+  .document('chats/{chatId}')
+  .onWrite((change, context) => {
+    console.log(`chat before ${change.before.data()} after ${change.after.data()}`)
+
+    return algoliaUpdate(chatsIndex, change)
+  });
+
+exports.algoliaMessages = functions.firestore
+  .document('chats/{chatId}/messages/{messageId}')
+  .onWrite((change, context) => {
+    console.log(`message before ${change.before.data()} after ${change.after.data()}`)
+
+    return algoliaUpdate(messagesIndex, change)
+  });
